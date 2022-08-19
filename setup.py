712d84a9091587853eda604ad3acc2021eee802d
Rob Moorman <rob@moori.nl>
2022-08-19 14:56:28 +0200

    version bump 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ docs_require = []
 
 setup(
     name="wagtail-cache-invalidator",
-    version="0.1.0",
+    version="0.2.0",
     description="",
     author="R.Moorman <rob@vicktor.nl>",
     install_requires=install_requires,
